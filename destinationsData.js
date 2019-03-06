176369c84801c6ad2f2f93f45423d764e4d8a4a0
Cristal Gomez <cristalsg1@gmail.com>
2019-03-06 14:48:55 -0600

    started

---
+++ destinationsData.js
@@ -0,0 +1,164 @@
+var destinationsArray = [
+	{
+		name: "Denver,CO",
+		photo: "denver.jpg",
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: "Walt Disney World (Orlando, Fl)",
+		photo: "",
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Las Vegas, NV',
+		photo: "",
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Seattle, WA',
+		photo: "",
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'New York City, NY',
+		photo: "",
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Los Angeles, CA',
+		photo: "",
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Nashville, TN',
+		photo: "",
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Austin, TX',
+		photo: "",
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'New Orleans, LA',
+		photo: "",
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Honolulu, HI',
+		photo: "",
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = destinationsArray;
---
+++ routes/apiRoutes.js
@@ -0,0 +1,1 @@
+var destinationsData = require("../data/destinationsData")
