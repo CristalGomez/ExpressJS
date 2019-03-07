a8f23c6baed4f774108b8f493a089b24d2812523
Cristal Gomez <cristalsg1@gmail.com>
2019-03-07 09:45:59 -0600

    changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,12 @@
 var destinationsData = require("../data/destinationsData")
+
+module.exports = function(app){
+    app.get("/api/destinations", function(req,res){
+        res.json(destinationsData)
+    })
+};
+
+app.post("/api/destinations", function(req, res){
+    destinationsData.push(req.body)
+});
+
