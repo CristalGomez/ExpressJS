b7968cba0a3a0fa6a5879425f2393a7f61f6c796
Cristal Gomez <cristalsg1@gmail.com>
2019-03-09 09:48:47 -0600

    fix for deployed css

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var express = require("express");
 var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
-
+app.use(express.static(path.join(__dirname, './public')))
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
