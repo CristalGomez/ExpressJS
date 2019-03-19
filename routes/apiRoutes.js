88df0a36be114ace77ca5038aca09a766362686d
Cristal Gomez <cristalsg1@gmail.com>
2019-03-19 00:09:17 -0500

    many changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,21 +3,40 @@ var destinationsData = require("../data/destinationsData.js")
 
 module.exports = function(app){
     app.get("/api/destinations", function(req,res){
-        res.json(destinations)
+        res.json(destinationsData)
     })
 
 app.post("/api/destinations", function(req, res){
+    var match = {
+        location: "",
+        image: "",
+        matchDifference: Infinity
+    }
+    //the user's survey results being parsed
     var userInput = req.body;
     var userScore = userInput.scores
 
-    var scoreDiff = 1000;
+    var scoreDifference;
+    //looping through all the matches in the db
+    for (var i = 0; i < destinationsData.length; i++){
+        var destination = destinationsData[i];
+        totalDifference = 0;
 
-    for (var i = 0; i < destinations.length; i++){
-        for (var x = 0; x < userInput.length; x++){
-            var compareScores = (Math.abs(destinations[i].scores) - (userScore[x]))
+        for (var x = 0; x < destination.scores.length; x++){
+            var destinationScore = destination.scores[x];
+            var userScores = userScores[x];
+
+            totalDifference += Math.abs(parseInt(userScores) - parseInt(destinationScore))
         }
 
+        if (totalDifference <= match.matchDifference){
+            match.location = destination.location;
+            match.image = destination.image;
+            match.matchDifference = totalDifference;
+        }
     }
+    destinations.push(userInput)
+    res.json(match)
 
 }); //end of app.post
 
